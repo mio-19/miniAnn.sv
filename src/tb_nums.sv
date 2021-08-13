ad53e0c96c18e5940ad444b32510085e37744562
ㄗㄠˋ ㄑㄧˊ <tsao-chi@the-lingo.org>
2021-08-13 11:13:35 +0000

    up

--- src/tb_nums.sv
+++ src/tb_nums.sv
@@ -8,9 +8,14 @@ module tb_nums;
         real_t c = real_add(a, b);
         $display("%f + %f = %f", real2real(a), real2real(b), real2real(c));
     endtask
+    task automatic test_sub_real(real_t a, real_t b);
+        real_t c = real_sub(a, b);
+        $display("%f + %f = %f", real2real(a), real2real(b), real2real(c));
+    endtask
 
     initial begin
         test_add_real(real_from_int(12),real_from_int(13));
+        test_sub_real(real_from_int(12),real_from_int(13));
     end
 
 endmodule
