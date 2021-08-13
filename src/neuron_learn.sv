6e8962680f216271e16af091b9435b473d190204
ㄗㄠˋ ㄑㄧˊ <tsao-chi@the-lingo.org>
2021-08-13 14:02:26 +0000

    up

--- src/neuron_learn.sv
+++ src/neuron_learn.sv
@@ -7,8 +7,11 @@ module neuron_learn #(
     input zero2one_t in [N-1:0],
     output zero2one_t out,
 
+    /* verilator lint_off UNOPTFLAT */
     output frac_t weights [N-1:0],
+    /* verilator lint_off UNOPTFLAT */
     output frac_t activation_max,
+    /* verilator lint_off UNOPTFLAT */
     output frac_t activation_min,
 
     input zero2one_t expected_out,
@@ -17,22 +20,20 @@ module neuron_learn #(
 
     neuron_run #(.N(N)) neuron_run_instance(.in(in), .out(out), .weights(weights), .activation_max(activation_max), .activation_min(activation_min));
 
+    /* verilator lint_off UNOPTFLAT */
     bit [N-1:0] random_v0;
-    bit [$clog2(`frac_t_size)-1:0] random_v1;
-    bit [$clog2(N)-1:0] random_v2;
+    frac_t random_v1;
     always @(valid, learn, in, expected_out) begin
         if (!valid) begin
             // randomly generate some values
-            random_v1[random_v0] <= random_v1[random_v0] ^ random_v0[~random_v2];
-            foreach (random_v0[i]) begin
-                random_v0[i] <= random_v0[i] ^ (random_v0[random_v2^i] ? weights[random_v2+i][random_v1-i] : in[random_v2-i][~random_v1+i]);
-            end
-            random_v2 <= random_v2 ^ ~random_v0;
             foreach (weights[i]) begin
-                weights[i] <= weights[i+random_v0] ^ {in[i+random_v1], out + random_v2};
+                if (random_v0[i]) weights[i] = weights[i] - random_v1;
+                if (in[i][0]) random_v0[i] = random_v0[i] ^ in[i][1];
+                if (in[i][1]) random_v0[i] = random_v0[i] ^ (~^ expected_out);
             end
-            activation_max <= activation_max + (random_v2 ^ random_v1);
-            activation_min <= activation_min - random_v0;
+            if (random_v0[0]) activation_max = activation_max + random_v1;
+            if (random_v0[1]) activation_min = activation_min ^ random_v1;
+            random_v1 = random_v1 + random_v0;
         end else begin
         end
     end
