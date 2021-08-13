2a420113bfac13a5592689586a14b3fc3620b424
ㄗㄠˋ ㄑㄧˊ <tsao-chi@the-lingo.org>
2021-08-13 10:32:55 +0000

    up

--- src/tb_nums.sv
+++ src/tb_nums.sv
@@ -1,12 +1,17 @@
 `include "defs.svh"
 module tb_nums;
 
+    function real real2real(real_t x);
+        real v = x;
+        real2real = v / (2^32);
+    endfunction
     task test_add_real(real_t a, real_t b);
         real_t c = real_add(a, b);
-        $display("");
+        $display("%f + %f = %f", real2real(a), real2real(b), real2real(c));
     endtask
 
     initial begin
+        test_add_real(real_from_i(12),real_from_i(13));
     end
 
 endmodule
