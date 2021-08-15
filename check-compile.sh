68df64738446ca5afef14723b09dcd30c4104408
ㄗㄠˋ ㄑㄧˊ <tsao-chi@the-lingo.org>
2021-08-15 08:33:31 +0000

    up

--- check-compile.sh
+++ check-compile.sh
@@ -2,5 +2,6 @@
 set -e
 cd "$(dirname "$0")"
 cd src
+rm -f *.v
 echo *.sv | xargs --max-args=1 --max-procs="$(nproc)" verilator --cc
 #echo *.sv | xargs --max-args=4 --max-procs="$(nproc)" verilator --cc
--- run-tb.sh
+++ run-tb.sh
@@ -2,10 +2,21 @@
 set -e
 cd "$(dirname "$0")"
 cd src
+run_sv(){
 for f in tb_*.sv; do
   echo "  --  $f  --"
   iverilog -g2005-sv -Y .sv -y . "$f"
   ./a.out
   rm a.out
 done
+}
+run_v(){
+for f in tb_*.v; do
+  echo "  --  $f  --"
+  iverilog -y . "$f"
+  ./a.out
+  rm a.out
+done
+}
+run_v
 echo Success!
